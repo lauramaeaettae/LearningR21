b0a4efb1a0f6283113130aa1930620e762ee2c3a
Laura Linnea Määttä <laura.maeaettae@gmail.com>
2021-06-16 09:58:58 +0200

    colnames, str, summary

--- R/wrangling-session.R
+++ R/wrangling-session.R
@@ -3,3 +3,13 @@ source(here::here("R/package_loading.R"))
 
 # Glimpse the NHANES dataset
 glimpse(NHANES)
+
+
+# Column names
+colnames(NHANES)
+
+# Structure
+str(NHANES)
+
+# Summary
+summary(NHANES)
