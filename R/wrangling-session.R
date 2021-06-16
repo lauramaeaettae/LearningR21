e088fb01d8fd4c67e9ccfb91a8616440baa866b1
Laura Linnea Määttä <laura.maeaettae@gmail.com>
2021-06-16 11:04:37 +0200

    selecting and piping

--- R/wrangling-session.R
+++ R/wrangling-session.R
@@ -4,7 +4,6 @@ source(here::here("R/package_loading.R"))
 # Glimpse the NHANES dataset
 glimpse(NHANES)
 
-
 # Column names
 colnames(NHANES)
 
@@ -13,3 +12,50 @@ str(NHANES)
 
 # Summary
 summary(NHANES)
+
+
+# Selecting columns
+select(NHANES, Age)
+
+select(NHANES, Age, Weight, BMI)
+
+# Exclude a column
+select(NHANES, -HeadCirc)
+
+# Selecting all columns starting with "BP"
+select(NHANES, starts_with("BP"))
+
+# Selecting columns ending with "day"
+select(NHANES, ends_with("Day"))
+
+# Selecting all columns with "age"
+select(NHANES, contains("Age"))
+
+?select_helpers # select help functions
+
+# Select and select a subset of the dataset
+nhanes_small <- select(NHANES, Age, Gender, Height, Weight, BMI, Diabetes,
+                       DiabetesAge, PhysActiveDays, PhysActive, TotChol, BPSysAve,
+                       BPDiaAve, SmokeNow, Poverty)
+nhanes_small
+
+## Renaming
+
+# Renaming to snake_case
+nhanes_small <- rename_with(nhanes_small, snakecase::to_snake_case)
+
+# Renaming specific columns
+rename(nhanes_small, sex = gender) # new name = old name
+# if you don't assign the action to an object it doesn't get saved
+nhanes_small <- rename(nhanes_small, sex = gender)
+
+## Learning to pipe %>%
+
+nhanes_small %>% colnames()
+# is equal to
+colnames(nhanes_small)
+
+# piping several functions together
+nhanes_small %>%
+    select(phys_active) %>%
+    rename(physicalle_active = phys_active)
